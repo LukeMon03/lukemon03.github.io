b8c6a8a29d2908a7c62f337072be2d751e46da7d
Luke-Monaghan <l.monaghan@ucas.ac.uk>
2022-09-14 15:15:48 +0100

    power, more power

---
+++ script.js
@@ -0,0 +1,13 @@
+// java script goes here
+function decideRaysFate(color) {
+    const footer = document.getElementById("Ray-has-no-power");
+    footer.style.color = color;
+    console.log(color);
+}
+
+    function move(state){
+    console.log(state)
+    const button = document.getElementById*("Dear-God-What-Have-You-done")
+    button.style.color = 'hotpink';
+}
+    
